3bad7baa40dceaba855530f95c2e85ff3bb4506b
Karim Languedoc <karimlanguedoc@outlook.com>
2023-04-12 22:38:15 -0400

    Replace files with framework

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "LinkedList",
+    platforms: [
+        .macOS(.v13), .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "LinkedList",
-            dependencies: []),
-        .testTarget(
-            name: "LinkedListTests",
-            dependencies: ["LinkedList"]),
+            path: "./Sources/LinkedList.xcframework"
+        )
     ]
 )
